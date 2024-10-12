8582fbd489d5b6235b5a16140838adee18c62757
zhuoying.li <zhuoying.li@transwarp.io>
2024-10-12 17:04:51 +0800

    fix:update

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,12 +7,18 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
-  -- { import = "astrocommunity.pack.python" },
-  -- { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.vue" },
   -- { import = "astrocommunity.pack.scala" },
-  { import = "astrocommunity.debugging" },
+  { import = "astrocommunity.quickfix" },
+  { import = "astrocommunity.debugging.nvim-chainsaw" },
+  { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+  { import = "astrocommunity.debugging.telescope-dap-nvim" },
   -- import/override with your plugins folder
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -7,6 +7,55 @@
 return {
 
   -- == Examples of Adding Plugins ==
+
+  -- lazy.nvim
+  {
+    "robitx/gp.nvim",
+    config = function()
+      local conf = {
+        -- For customization, refer to Install > Configuration in the Documentation/Readme
+        providers = {
+          openai = {
+            endpoint = "https://api.openai.com/v1/chat/completions",
+            secret = os.getenv "OPENAI_API_KEY",
+          },
+
+          -- azure = {...},
+
+          copilot = {
+            endpoint = "https://api.githubcopilot.com/chat/completions",
+            secret = {
+              "bash",
+              "-c",
+              "cat ~/.config/github-copilot/hosts.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+            },
+          },
+
+          pplx = {
+            endpoint = "https://api.perplexity.ai/chat/completions",
+            secret = os.getenv "PPLX_API_KEY",
+          },
+
+          ollama = {
+            endpoint = "http://localhost:11434/v1/chat/completions",
+          },
+
+          googleai = {
+            endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
+            secret = os.getenv "GOOGLEAI_API_KEY",
+          },
+
+          anthropic = {
+            endpoint = "https://api.anthropic.com/v1/messages",
+            secret = os.getenv "ANTHROPIC_API_KEY",
+          },
+        },
+      }
+      require("gp").setup(conf)
+
+      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+    end,
+  },
   {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
