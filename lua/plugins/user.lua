5d268f8255801a63612ba7da98a22f9f3fc92424
zhuoying.li <leejoy@mail.ustc.edu.cn>
2025-05-14 23:30:48 +0800

    feat(lazygit): add lazygit plugin support

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -5,7 +5,50 @@
 
 ---@type LazySpec
 return {
-
+  { "augmentcode/augment.vim" },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
+  -- {
+  --   "Exafunction/windsurf.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   config = function() require("codeium").setup {} end,
+  -- },
+  {
+    "Exafunction/windsurf.vim",
+    event = "BufEnter",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+      vim.keymap.set(
+        "i",
+        "<c-;>",
+        function() return vim.fn["codeium#CycleCompletions"](1) end,
+        { expr = true, silent = true }
+      )
+      vim.keymap.set(
+        "i",
+        "<c-,>",
+        function() return vim.fn["codeium#CycleCompletions"](-1) end,
+        { expr = true, silent = true }
+      )
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+    end,
+  },
   -- == Examples of Adding Plugins ==
   {
     "rmagatti/auto-session",
--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -2,10 +2,11 @@ return {
   "gelguy/wilder.nvim",
   config = function()
     -- config goes here
+    -- vim.cmd "UpdateRemotePlugins"
     require("wilder").setup { modes = { ":", "/", "?" } }
     require("wilder").set_option("pipeline", {
-      require('wilder').branch(
-        require('wilder').cmdline_pipeline {
+      require("wilder").branch(
+        require("wilder").cmdline_pipeline {
           -- sets the language to use, 'vim' and 'python' are supported
           language = "python",
           -- 0 turns off fuzzy matching
@@ -13,11 +14,11 @@ return {
           -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
           fuzzy = 1,
         },
-        require('wilder').python_search_pipeline {
+        require("wilder").python_search_pipeline {
           -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-          pattern =require('wilder').python_fuzzy_pattern(),
+          pattern = require("wilder").python_fuzzy_pattern(),
           -- omit to get results in the order they appear in the buffer
-          sorter = require('wilder').python_difflib_sorter(),
+          sorter = require("wilder").python_difflib_sorter(),
           -- can be set to 're2' for performance, requires pyre2 to be installed
           -- see :h wilder#python_search() for more details
           engine = "re",
