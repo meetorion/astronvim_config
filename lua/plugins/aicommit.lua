f1d69a173e525367a38ee9495db164f3dab28146
zhuoying.li <leejoy@mail.ustc.edu.cn>
2025-06-06 14:00:08 +0800

    2. **refactor(aicommit): update plugin source and maintain dependencies**

--- lua/plugins/aicommit.lua
+++ lua/plugins/aicommit.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "vernette/ai-commit.nvim",
+    "meetorion/ai-commit.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
@@ -10,8 +10,8 @@ return {
         {
           openrouter_api_key = os.getenv "OPENROUTER_API_KEY", -- or set OPENROUTER_API_KEY environment variable
           -- model = "qwen/qwen-2.5-72b-instruct:free", -- default model
-          -- model = "anthropic/claude-3.5-sonnet",
-          model = "deepseek/deepseek-chat-v3-0324",
+          model = "anthropic/claude-3.5-sonnet",
+          -- model = "deepseek/deepseek-chat-v3-0324",
           -- model = "microsoft/phi-3-mini-128k-instruct",
           auto_push = true, -- whether to automatically push after commit
         },
