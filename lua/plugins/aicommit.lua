1b8658f0bfe76f490da229d1aa08411d4caf879b
zhuoying.li <leejoy@mail.ustc.edu.cn>
2025-06-06 10:59:08 +0800

    feat(aicommit): update model to microsoft/phi-3-mini-128k-instruct

--- lua/plugins/aicommit.lua
+++ lua/plugins/aicommit.lua
@@ -10,7 +10,8 @@ return {
         {
           openrouter_api_key = os.getenv "OPENROUTER_API_KEY", -- or set OPENROUTER_API_KEY environment variable
           -- model = "qwen/qwen-2.5-72b-instruct:free", -- default model
-          model = "anthropic/claude-3.5-sonnet",
+          -- model = "anthropic/claude-3.5-sonnet",
+          model = "microsoft/phi-3-mini-128k-instruct",
           auto_push = true, -- whether to automatically push after commit
         },
       }
